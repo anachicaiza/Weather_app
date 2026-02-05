5e24ed269e59de8f82604f94dd77f0cd5e557cf7
Ana Chicaiza <anachicaiza@icloud.com>
2026-02-05 16:11:57 +0000

    Axios  imported.
    City and temperature are targeted from JS  using API

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", searchSubmit);
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+
+function searchCity(city) {
+    let apiKey = "b739b64actfb7710ab2aa8f6044o4c38";
+    let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiURL).then(refreshWeather);
+}
 
 function searchSubmit(event) {
     event.preventDefault();
     let inputElement = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = inputElement.value;
+    searchCity(inputElement.value);
 }
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", searchSubmit);
+
+searchCity("London");
