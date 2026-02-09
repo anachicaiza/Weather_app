52acc39b077b5d1ac8b52b37c8e6c144ca48577d
Ana Chicaiza <anachicaiza@icloud.com>
2026-02-09 23:14:14 +0000

    displayForecast  function created
    Weather from  Tue to Sat using API data weather

--- index.js
+++ index.js
@@ -29,6 +29,8 @@ function refreshWeather(response) {
     windElement.innerHTML = wind;
     timeElement.innerHTML = formatDate(date);
     iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="icon" />`;
+
+    getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -41,7 +43,7 @@ function formatDate(date) {
         minutes = `0${minutes}`;
     }
 
-    return `${day} ${hours}:${minutes}, `;
+    return `${day} ${hours}:${minutes}`;
 }
 
 
@@ -57,7 +59,14 @@ function searchSubmit(event) {
     searchCity(inputElement.value);
 }
 
-function displayForecast() {
+function getForecast(city) {
+    let apiKey = "b739b64actfb7710ab2aa8f6044o4c38";
+    let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiURL).then(displayForecast);
+}
+
+function displayForecast(response) {
+    console.log(response.data)
     let forecastElement = document.querySelector("#forecast");
     let days = ["Tues", "Wed", "Thu", "Fri", "Sat"];
     let forecastHtml = "";
@@ -67,7 +76,7 @@ function displayForecast() {
             forecast.innerHTML +
             `
     <div class="forecast-day">
-            <div class="forecast-date">Wed</div>
+            <div class="forecast-date">${day}</div>
             <div class="forecast-icon"></div>
             <div class="forecast-temperatures">
                 <div class="forecast-temperature"><strong>18°</strong></div>
@@ -85,4 +94,3 @@ let form = document.querySelector("#search-form");
 form.addEventListener("submit", searchSubmit);
 
 searchCity("London");
-displayForecast();
